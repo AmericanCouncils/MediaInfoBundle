b5d9f03c73bf8909c8cb9f791dbaa7f603765885
Evan Villemez <evan.villemez@americancouncils.org>
2013-07-30 10:17:10 -0400

    fix cs

--- Command/MediaInfoCommand.php
+++ Command/MediaInfoCommand.php
@@ -26,31 +26,31 @@ class MediaInfoCommand extends ContainerAwareCommand
                 new InputOption('format', null, InputOption::VALUE_REQUIRED, 'Optionally specify alternate output format (json/yaml/xml/array).', 'yaml')
             ));
     }
-    
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $format = $input->getOption('format');
         if (!in_array($format, array('json','yaml','array'))) {
             throw new \InvalidArgumentException("Invalid format chosen.");
         }
-                
+
         $mediainfo = $this->getContainer()->get('ac.mediainfo');
-        
+
         $data = $mediainfo->scan($input->getArgument('path'), $format);
-        
+
         if ('array' === $format) {
             $data = print_r($data, true);
         }
-        
+
         if ('json' === $format) {
             $data = json_encode($data);
         }
-        
+
         if ('yaml' === $format) {
             $dumper = new Dumper();
             $data = $dumper->dump($data, 5);
         }
-        
+
         $output->write($data);
     }
 }
--- MediaInfo.php
+++ MediaInfo.php
@@ -13,7 +13,7 @@ use Symfony\Component\Process\Process;
 class MediaInfo
 {
     protected $path;
-    
+
     /**
      * Constructor requires path to mediainfo executable.
      *
@@ -23,33 +23,33 @@ class MediaInfo
     {
         $this->path = $path;
     }
-    
+
     /**
      * Scans a file to return any structured metadata.
      *
-     * @param string $filepath Path to file to analyze
+     * @param  string $filepath Path to file to analyze
      * @return array
      */
     public function scan($filepath)
     {
         $proc = new Process(sprintf('%s %s --Output=XML --Full', $this->path, $filepath));
         $proc->run();
-        
+
         if (!$proc->isSuccessful()) {
             throw new \RuntimeException($proc->getErrorOutput());
         }
-        
+
         $result = $proc->getOutput();
-        
+
         //parse structure to turn into raw php array
         $xml = simplexml_load_string($result);
         $data = array();
         $data['version'] = (string) $xml['version'];
         $data['file'] = array();
-        
+
         foreach ($xml->File->track as $track) {
             $data['file'][strtolower($track['type'])] = array();
-            
+
             foreach ($track as $key => $val) {
                 $data['file'][strtolower($track['type'])][strtolower($key)] = (string) $val;
             }
